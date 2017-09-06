bff95fe97cbfa2857370c511ee0186c020ac48f2
zhashaonan--company <1479474496@qq.com>
2017-09-06 14:54:35 +0800

    multi-pages

---
+++ mulit-pages/webpack.config.js
@@ -0,0 +1,89 @@
+var webpack = require('webpack')
+var path = require('path')
+var CleanWebpackPlugin = require('clean-webpack-plugin')
+var UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+var HtmlWebpackPlugin = require('html-webpack-plugin')
+var ExtractTextPlugin = require('extract-text-webpack-plugin')
+var CopyWebpackPlugin = require('copy-webpack-plugin')
+    // var extractCSS = new ExtractTextPlugin('[name].bundle.css')
+
+module.exports = {
+    devtool: 'cheap-module-eval-source-map',
+    entry: {
+        main: __dirname + '/app/main.js',
+        vendor: ['jquery', 'moment']
+    },
+    output: {
+        path: __dirname + '/public',
+        filename: 'static/js/[name].[id].js',
+        publicPath: '/'
+    },
+    module: {
+        rules: [{
+                test: /\.js$/, //解析文件类型
+                exclude: /node_modules/, //排除mode_modules文件
+                loader: 'babel-loader', //使用哪种loader解析
+                query: {
+                    presets: ['es2015', 'stage-0'] //loader的配置项，解析es6
+                }
+            },
+            {
+                test: /\.(png|jpe?g|gif|svg)(\?.*)?$/,
+                loader: 'url-loader',
+                options: {
+                    limit: 8,
+                    name: 'static/img/[name].[hash:7].[ext]'
+                }
+            },
+            {
+                test: /\.(less|css)$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: 'style-loader',
+                    use: [
+                        { loader: 'css-loader', options: { importLoaders: 1, minimize: true } },
+                        'postcss-loader'
+                    ]
+                })
+            }
+        ]
+    },
+    plugins: [
+        new CleanWebpackPlugin(
+            ['public/js/main.*.js', 'public/js/manifest.*.js', 'public/css/*.*.css'], {
+                root: __dirname,
+                verbose: true,
+                dry: false
+            }
+        ),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            names: ['vendor', 'manifest']
+        }),
+        new ExtractTextPlugin("static/css/[name].bundle.css"),
+        new UglifyJsPlugin({
+            beautify: true,
+            exclude: ['/node_modules/'],
+            compress: {
+                warnings: false
+            },
+            output: {
+                comments: false
+            }
+        }),
+        new HtmlWebpackPlugin({
+            title: 'webpack-demo',
+            template: 'index.html'
+        }),
+        // copy custom static assets
+        new CopyWebpackPlugin([{
+            from: path.resolve(__dirname + '/static'),
+            to: path.resolve(__dirname + '/public/static'),
+            ignore: ['.*']
+        }])
+    ],
+    devServer: {
+        inline: true,
+        hot: true,
+        port: 8181
+    }
+}
